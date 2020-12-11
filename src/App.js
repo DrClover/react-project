c02eabe872e7b0c4fc9cae602900ba18270cdd16
ajie <ajiephangzak@gmail.com>
2020-12-11 17:48:11 +0800

    push to github account

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// Core
+import React from "react";
+import './App.css'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="app">
+        anything
+        
+      </div>
+    );
+  }
+}
+
+
+export default App;
